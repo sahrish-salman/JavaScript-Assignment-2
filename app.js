7327e77d0c4192168c0540f8cf1d0df4463b67ed
sahrish-salman <sahrish.salman@gmail.com>
2022-11-05 14:43:22 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,424 @@
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+
+// console.log(spec);
+
+// var numberOfCats = prompt("How many cats?");
+// var tooManyCats = numberOfCats + 1;
+
+// console.log(tooManyCats);
+
+// var x = prompt("Where does the Pope live?");
+// var correctAnswer = "Vatican";
+
+// if (x === correctAnswer) {
+// alert("Correct!")
+// }
+
+// console.log(x);
+
+// var x = prompt("Where does the Pope live?");
+// var correctAnswer = "Vatican";
+// var score= 10 ;
+// var newscore = ++score ;
+
+// if (x === correctAnswer) {
+//    alert("Correct!")
+//    console.log(newscore);
+// }
+
+// var x = prompt("Where does the Pope live?");
+// var correctAnswer = "Vatican";
+// userIQ = "genius";
+
+// if (x === correctAnswer) {
+//     console.log(userIQ);
+// }
+
+
+// var FirstName = prompt("write first Name");
+// var MidName = prompt("write mid name");
+// var LastName = prompt("write last name")
+// var fullName = FirstName+ MidName+ LastName;
+
+// if (fullName === "Mark"+ "Tony" + "Myers") {
+//    console.log("Welcome") ;
+// } else {
+//     console.log("error");
+// }
+
+// var materialsCost = +prompt("write materialsCost");
+// var deliveryCost = +prompt("write deliveryCost");
+// var totalCost= materialsCost+deliveryCost;
+
+// if (totalCost === 81.50 + 135) {
+//     console.log("Approved");
+// } else {
+//     console.log("Not Approved");
+// }
+
+// var TotalCost= 150;
+// var materialsCost= +prompt("write materialCost");
+// var laborCost = +prompt("write labor cost");
+
+// if (TotalCost === materialsCost + laborCost) {
+//     console.log("Approved");
+// } else {
+//     console.log("Not Approved");
+// }
+
+
+// var x = 20;
+// var y = 10;
+// var a = 40;
+// var b = 10;
+
+// if (x + y === a - b) {
+//     console.log("good");
+// } else {
+//     console.log("wrong");
+// }
+
+
+// var x = 10;
+// var y = 10;
+// var a = 40;
+// var b = 10;
+
+// if (x + y === a - b) {
+//     console.log("good");
+// } else {
+//     console.log("wrong");
+// }
+
+// var YourTicketNumber = 487208;
+// var YourTicketNumber = prompt("enter your ticket number ");
+
+// if (YourTicketNumber !== 487208) {
+//     alert("Better luck next time.");
+// }
+
+
+// var a = 10;
+// var b = 9;
+
+// if (a > b) {
+//     console.log("A is greater than B");
+// }
+
+// var a = 10;
+// var b = 11;
+
+// if (a < b) {
+//     console.log("A is less than B");
+// }
+
+// var a = 12;
+// var b = 11;
+
+// if (a >= b) {
+//     console.log("A is greater than or equal to B");
+// }
+
+// var a = 12;
+// var b = 12;
+
+// if (a >= b) {
+//     console.log("A is greater than or equal to B");
+// }
+
+
+// var a = 11;
+// var b = 12;
+
+// if (a <= b) {
+//     console.log("A is less than or equal to B");
+// }
+
+// var a = 11;
+// var b = 11;
+
+// if (a <= b) {
+//     console.log("A is less than or equal to B");
+// }
+
+// var x = prompt("Where does the Pope live?");
+
+// if (x === "Vatican") {
+// alert("Correct!");
+// }
+// if (x !== "Vatican") {
+// alert("Wrong answer");
+// }
+
+// var x = prompt("Where does the Pope live?");
+
+// if (x === "Vatican") {
+// alert("Correct!");
+// }
+// else {
+// alert("Wrong answer");
+// }
+
+// var correctAnswer = "Vatican";
+// var x = prompt("Where does the Pope live?");
+// var userIQ = "problematic";
+// var score = 10;
+// var newscore = --score ;
+
+// if (x === correctAnswer) {
+// alert("Correct!");
+// }
+// else {
+// console.log(newscore);
+// console.log(userIQ); 
+// alert("Incorrect");
+// }
+
+// var correctAnswer = "Vatican";
+// var x = prompt ("Where does the pope live");
+
+// if (x === correctAnswer) {
+//     alert("Correct!");
+// }
+// else if (x === "Rome") {
+// alert("Incorrect but close");
+// }
+// else {
+// alert("Incorrect");
+// }
+
+// var weight = 301;
+// time = 5;
+
+// if (weight > 300 && time < 6) {
+//     alert("Come to our tryout!");
+//     }
+//     else {
+//     alert("Come to our cookout!");
+//     }
+
+// var weight = 299;
+// time = 5;
+
+// if (weight > 300 && time < 6) {
+//     alert("Come to our tryout!");
+//     }
+//     else {
+//     alert("Come to our cookout!");
+//     }
+
+// var weight = 301;
+// var time = 5;
+// var age = 18;
+// gender = "male";
+
+// if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//     alert("Come to our tryout!");
+//     }
+//     else {
+//     alert("Come to our cookout!");
+//     }
+
+// var weight = 30;
+// var time = 6;
+// var age = 18;
+// gender = "male";
+
+// if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//     alert("Come to our tryout!");
+//     }
+//     else {
+//     alert("Come to our cookout!");
+//     }
+
+// var SAT = 10;
+// var avg = 9;
+// var GPA = 2;
+// var sport = "football";
+
+// if (SAT > avg || GPA > 2.5 || sport === "football") {
+//     alert("Welcome to Bubba State!");
+// }
+// else {
+//     alert("Have you looked into appliance repair?");
+// }
+
+// var SAT = 10;
+// var avg = 10;
+// var GPA = 3;
+// var sport = "football";
+
+// if (SAT > avg || GPA > 2.5 || sport === "football") {
+//     alert("Welcome to Bubba State!");
+// }
+// else {
+//     alert("Have you looked into appliance repair?");
+// }
+
+// var SAT = 10;
+// var avg = 10;
+// var GPA = 2;
+// var sport = "hokey";
+
+// if (SAT > avg || GPA > 2.5 || sport === "football") {
+//     alert("Welcome to Bubba State!");
+// }
+// else {
+//     alert("Have you looked into appliance repair?");
+// }
+
+// var age = 66;
+// var res = "U.S.";
+
+// if (age > 65 || age < 21 && res === "U.S.") {
+//     console.log("Pass");
+// }
+
+// var age = 66;
+// var res = "U.S.";
+
+// if (age < 65 || age < 21 && res === "U.S.") {
+//     console.log("Pass");
+// } else {
+//     console.log("Not Pass");
+// }
+
+// var age = 20;
+// var res = "U.S.";
+
+// if ((age > 65 || age < 21) && res === "U.S.") {
+//     console.log("Pass");
+// }
+
+// var age = 22;
+// var res = "U.S.";
+
+// if (age > 65 || (age < 21 && res === "U.S.")) {
+//     console.log("Pass");
+// }  else {
+//     console.log("Not Pass");
+// }
+
+// var age = 20;
+// var res = "U.S.";
+
+// if (age > 65 || (age < 21 && res === "U.S.")) {
+//     console.log("Pass");
+// } else {
+//     console.log("Not Pass");
+// }
+
+// var x = 10;
+// var y = 10;
+// var a = 9;
+// var b = 8;
+// var c = 20;
+// var d = 20;
+// var g = "Pass";
+// var h = g ;
+// var e = "Not Pass";
+// var f = e ;
+
+// if ((x === y || a === b) && c === d) {
+//     g = h;
+//     console.log(h);
+// }
+// else { e = f;
+//     console.log(f);
+// }
+
+// var x = 10;
+// var y = 10;
+// var a = 9;
+// var b = 8;
+// var c = 22;
+// var d = 20;
+// var g = "Pass";
+// var h = g ;
+// var e = "Not Pass";
+// var f = e;
+
+// if ((x === y || a === b) && c === d) {
+//     g = h;
+//     console.log(h);
+// }
+// else { e = f;
+//     console.log(f);
+// }
+
+// var c = 5;
+// var d = 4;
+// var a = 8;
+// var b = 8;
+// var g = "Flase";
+// var h = g ;
+// var e = "True";
+// var f = e;
+
+
+// if (c === d) {
+//     g = h;
+//     console.log(h);
+// }
+// else {
+//     if (a === b) {
+//     g = h;
+//     console.log(h);
+//     }
+//     else {
+//     e = f;
+//     console.log(f);
+//     }
+// }
+
+// var c = 5;
+// var d = 5;
+// var a = 7;
+// var b = 8;
+// var g = "Flase";
+// var h = g ;
+// var e = "True";
+// var f = e;
+
+
+// if (c === d) {
+//     g = h;
+//     console.log(h);
+// }
+// else {
+//     if (a === b) {
+//     g = h;
+//     console.log(h);
+//     }
+//     else {
+//     e = f;
+//     console.log(f);
+//     }
+// }
+
+// var c = 4;
+// var d = 5;
+// var a = 7;
+// var b = 8;
+// var g = "Flase";
+// var h = g ;
+// var e = "True";
+// var f = e;
+
+
+// if (c === d) {
+//     g = h;
+//     console.log(h);
+// }
+// else {
+//     if (a === b) {
+//     g = h;
+//     console.log(h);
+//     }
+//     else {
+//     e = f;
+//     console.log(f);
+//     }
+// }
